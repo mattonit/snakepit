bae972dc4b3a1a4dcd5473fa4dfc5b6ad99e98a6
mattonit <malan95@gmail.com>
2020-10-17 14:43:54 +0200

    added some styles for home page

--- src/docs/pages/home/index.js
+++ src/docs/pages/home/index.js
@@ -4,43 +4,48 @@ import {Wrapper, CellGroup, Cell, Heading, Button, Text} from 'snakepit';
 
 const HomePage = () => {
   return (
-    <Wrapper>
+    <div>
+      <section className="docs-intro">
 
-      <Heading level="1">Home</Heading>
-      <Text>Snakepit - modern UI library for React.js</Text>
-      <Link to="/docs/getting-started/">Getting Started</Link>
+      </section>
+      <Wrapper>
 
-      <CellGroup>
-        <Cell size="16" className="mt-3">
-          <Button color="primary">Primary</Button>
-          <Button color="accent">Accent</Button>
-          <Button color="info">Info</Button>
-          <Button color="success">Success</Button>
-          <Button color="danger">Danger</Button>
-          <Button color="warning">Warning</Button>
-        </Cell>
+        <Heading level="1">Home</Heading>
+        <Text>Snakepit - modern UI library for React.js</Text>
+        <Link to="/docs/getting-started/">Getting Started</Link>
 
-        <Cell size="16" className="mt-3">
-          <Button color="primary" outline>Primary</Button>
-          <Button color="accent" outline>Accent</Button>
-          <Button color="info" outline>Info</Button>
-          <Button color="success" outline>Success</Button>
-          <Button color="danger" outline>Danger</Button>
-          <Button color="warning" outline>Warning</Button>
-        </Cell>
+        <CellGroup>
+          <Cell size="16" className="mt-3">
+            <Button color="primary">Primary</Button>
+            <Button color="accent">Accent</Button>
+            <Button color="info">Info</Button>
+            <Button color="success">Success</Button>
+            <Button color="danger">Danger</Button>
+            <Button color="warning">Warning</Button>
+          </Cell>
 
-        <Cell size="16" className="mt-3">
-          <Button color="primary" inverse>Primary</Button>
-          <Button color="accent" inverse>Accent</Button>
-          <Button color="info" inverse>Info</Button>
-          <Button color="success" inverse>Success</Button>
-          <Button color="danger" inverse>Danger</Button>
-          <Button color="warning" inverse>Warning</Button>
-        </Cell>
+          <Cell size="16" className="mt-3">
+            <Button color="primary" outline>Primary</Button>
+            <Button color="accent" outline>Accent</Button>
+            <Button color="info" outline>Info</Button>
+            <Button color="success" outline>Success</Button>
+            <Button color="danger" outline>Danger</Button>
+            <Button color="warning" outline>Warning</Button>
+          </Cell>
 
-      </CellGroup>
+          <Cell size="16" className="mt-3">
+            <Button color="primary" inverse>Primary</Button>
+            <Button color="accent" inverse>Accent</Button>
+            <Button color="info" inverse>Info</Button>
+            <Button color="success" inverse>Success</Button>
+            <Button color="danger" inverse>Danger</Button>
+            <Button color="warning" inverse>Warning</Button>
+          </Cell>
 
-    </Wrapper>
+        </CellGroup>
+
+      </Wrapper>
+    </div>
 
   );
 };
